30da441f94712f06a80f3e63a1bb1469be12cc82
johnduplito <johnduplito@gmail.com>
2026-05-07 16:43:33 +1000

    Guard against injecting scripts on restricted browser URLs

--- background.js
+++ background.js
@@ -1,4 +1,7 @@
 chrome.action.onClicked.addListener(async (tab) => {
+  if (!tab.url || /^(chrome|edge|about|data):/.test(tab.url) || tab.url.startsWith('https://chromewebstore.google.com')) {
+    return;
+  }
   await chrome.scripting.insertCSS({
     target: { tabId: tab.id },
     files: ['civ.css'],
